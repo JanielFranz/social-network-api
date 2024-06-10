0ebb96a0d099e544535e39c048192d304da7b65c
JanielFranz <102563480+JanielFranz@users.noreply.github.com>
2024-06-10 04:11:24 -0500

    chore: first steps

---
+++ digital-solutions/ConsoleApp/Program.cs
@@ -0,0 +1,17 @@
+﻿
+using Newtonsoft.Json.Linq;
+
+using (var client = new HttpClient())
+{
+    string url = "http://localhost:5197/api/v1/status/Pepe";
+    client.DefaultRequestHeaders.Clear();
+    var response = client.GetAsync(url).Result;
+    var res = response.Content.ReadAsStringAsync().Result;
+    dynamic r = JArray.Parse(res);
+    
+    foreach (JObject item in r)
+    {
+        string message = (string)item["message"];
+        Console.WriteLine(message);
+    }
+}
