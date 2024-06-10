df33252e0d43b4fc82caefedf4ca8dfac0bdd930
JanielFranz <102563480+JanielFranz@users.noreply.github.com>
2024-06-10 14:41:23 -0500

    fix: server error fixed

--- digital-solutions.Console/ConsoleApp/IUserBehavior.cs
+++
@@ -1,8 +0,0 @@
-namespace program;
-
-public interface IUserBehavior
-{
-    public  Task Post(string mensaje, string usuario);
-    public Task Follow(string follower, string followed);
-    public Task Dashboard(string follower);
-}
